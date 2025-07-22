34fadefdfbc4f05270f7005d9b657a25a27593c0
radeet <greenglass0k@gmail.com>
2025-07-22 20:47:27 +0800

    arithmatic_operatorssss

--- arithmetic_operation.c
+++ arithmetic_operation.c
@@ -4,7 +4,7 @@ int main () {
     // OPERATOR ARITMATIKA 
     // = + - * / % ++ --
     
-    int x = 2;
+    int x = 10;
     int y = 3;
 
     // int z akan digunakan untuk menyimpan hasil aritmatika pada int x dan y
@@ -23,19 +23,38 @@ int main () {
     // printf("\n%d", z);
 
     // selanjutnya adalah pembagian, operasi pembagian akan lumayan aneh ketika bekerja pada tipe data int
-    z = x / y;
-    printf("\n%d", z);
-    printf("\nHasil yang keluar pada terminal menunjukan 0, padahal hasil pembagian dari\n2/3 adalah 0.66666....\n");
-    printf("\nhal tersebut adalah karena int tidak dapat menyimpan koma, maka dari itu\njika sebuah int dibagi dengan sebuah float/selain int lain, hasil yang didapatkan baru akan sesuai");
-    printf("\nberikut adalah contohnya, pembagian variable r (int) dengan e (float)\n");
+//     z = x / y;
+//     printf("\n%d", z);
+//     printf("\nHasil yang keluar pada terminal menunjukan 0, padahal hasil pembagian dari\n2/3 adalah 0.66666....\n");
+//     printf("\nhal tersebut adalah karena int tidak dapat menyimpan koma, maka dari itu\njika sebuah int dibagi dengan sebuah float/selain int lain, hasil yang didapatkan baru akan sesuai");
+//     printf("\nberikut adalah contohnya, pembagian variable r (int) dengan e (float)\n");
 
- int r = 2;
- float e = 3;
- float f = 0;
+//  int r = 2;
+//  float e = 3;
+//  float f = 0;
 
- f = r/e;
- printf("\n%f", f);
- printf("\n\nvariable r adalah int\nvariable e adalah float\n");
+//  f = r/e;
+//  printf("\n%f", f);
+//  printf("\n\nvariable r adalah int\nvariable e adalah float\n");
+
+    // penggunaan % akan menampilkan sisa dari suatu pembagian ( modulus )
+    // z = x%y;
+    // printf("\n%d", z);
+
+    // selanjutnya adalah increment ++, langkahnya juga sama ketika menggunakan --.
+    // x++;
+    // printf("\n%d",x);
+
+    // berikutnya adalah augmented assigment operator
+    // yaitu mengubah langsung nilai variable nya seperti contoh berikut 
+
+    // x+=10;
+    // x-=10;
+    // x/=10;
+    // x*=10;
+
+    // printf("\n%d",x);
+
     
 
 
