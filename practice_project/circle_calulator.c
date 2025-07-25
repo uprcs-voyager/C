5b85b033e85938d1dee98447f33a4cfb3c5b9270
radeet <greenglass0k@gmail.com>
2025-07-25 21:40:53 +0800

    circle calculator done

--- practice_project/circle_calulator.c
+++ practice_project/circle_calulator.c
@@ -4,6 +4,13 @@
 int main () {   
     const double PI  = 3.1415926535; // untuk penamaan konstan biasakan menggunakan huruf kapital
     double circle_radius = 0.0;
+    // User input 
+    printf("\n/////// Halo selamat datang ///////\n\n");
+
+    printf("Tolong masukan nilai jari-jari lingkaran (nilai akan dihitung dalam cm): \n");
+    scanf("%lf", &circle_radius);
+
+    
 
     // CALCULATE CIRCLE AREA 
     double area = PI*(pow(circle_radius, 2));
@@ -14,13 +21,10 @@ int main () {
     // CALCULATE SPHERE VOLUME 
     double volume = (4.0/3.0)*PI*(pow(circle_radius, 3));
 
-    // User input 
-    printf("\n/////// Halo selamat datang ///////\n\n");
 
-    printf("Tolong masukan nilai jari-jari lingkaran: ");
-    scanf("%lf", &circle_radius);
-
-    printf("")
+    printf("Luas lingkaran tersebut adalah: %.2lfcm\n", area);
+    printf("Luas permukaan bola tersebut adalah: %.2lfcm", surface_area);
+    printf("\nVolume dari bola tersebut adalah: %.2lfcm", volume);
 
     return 0;
 }
