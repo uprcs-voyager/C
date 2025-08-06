ebf1f37dce0d4d53ed6d32d1f9fc3e946ab41aea
radeet <greenglass0k@gmail.com>
2025-08-06 14:52:52 +0800

    saving da progres

---
+++ practice_project/compound_interest_calculator.c
@@ -0,0 +1,25 @@
+// KALKULATOR BUNGA MAJEMUK
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main () {
+
+    float modal_awal;
+    float bunga = 0.0;
+    float persentase_bunga = bunga/100;
+
+    printf("///// kalkulator bunga majemuk /////\n\n");
+    printf("Berapa modal awal anda? (Ketik dalam bentuk angka)\nContoh (250000): ");
+    scanf("%f", &modal_awal);
+    // printf("modal awal anda adalah: %.1f", modal_awal); debugging
+
+
+
+
+
+
+
+    return 0;
+}
