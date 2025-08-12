81928c1ceda2f7be0a9303152cb8d7ecec955e6c
radeet <greenglass0k@gmail.com>
2025-08-12 14:21:17 +0800

    logical_operator

---
+++ logical_operator.c
@@ -0,0 +1,32 @@
+// LOGICAL OPERATOR
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main () {
+    // logical operators are
+    // &&   = gunakan jika 2 kondisi harus bernilai true
+    // ||   = gunakan jika salah satu nilai bisa bernilai true, atau jika ingin menggunakan range
+    // if (choice == 'Y'  || choice == 'y') {
+    //             Result = FirstNumber/SecondNumber;
+    //             printf("Hasil dari pembagian %.2lf dan %.2lf adalah: %.2lf", FirstNumber, SecondNumber, Result);
+    //         } 
+    //         else if (choice == 'n' || choice == 'N'){
+    //             printf("\nTolong jangan Bagi dengan nol");
+    //         } 
+
+    // ! = not
+    // bool isSunny = false;
+    // if(!isSunny) {
+    //     printf("\nIt's cloudy here");
+    // } else if (isSunny)
+    // {
+    //     printf("\nIt is Sunny");
+    // }
+    
+    
+
+
+
+    return 0;
+}
