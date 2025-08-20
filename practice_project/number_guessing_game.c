9d86a698688b2ec55796015264e67eec4591121b
radeet <greenglass0k@gmail.com>
2025-08-20 17:54:41 +0800

    number guessing game done

--- practice_project/number_guessing_game.c
+++ practice_project/number_guessing_game.c
@@ -21,28 +21,29 @@ int main () {
     printf("\n.");
     Sleep(2000);
     printf("\n.");
-    printf("\n\nKomputer sudah menentukan angka !!\n");
-    printf("Coba tebak angka berapa yang sudah di tentukan (masukan dalam bentuk angka): ");
-    scanf("%d", &guess);
+    printf("\n\nKomputer sudah menentukan angka (1-100) !!\n");
 
-    while (guess != answer)
-    {
-        if(guess < answer) {
-            printf("\n Jawaban anda terlalu rendah!!");
-            printf("\ncoba tebak lagi: ");
-            scanf(" %d", &guess);
-            attempt++;
+
+
+    do {
+         printf("Coba tebak angka berapa yang sudah di tentukan (masukan dalam bentuk angka): ");
+         scanf("%d", &guess);
+         attempt++;
+
+      if(guess < answer) {
+            printf("\nJawaban anda terlalu rendah!!\n");
         }  else if (guess > answer) {
-                printf("\n Jawaban anda terlalu Tinggi!!");
-                printf("\ncoba tebak lagi: ");
-                scanf(" %d", &guess);
-            attempt++;
-        } else if  (guess == answer) {
-            printf("\n jawaban anda %d benar!! angka yang benar adalah: %d", guess, answer);
-            printf("jumlah percobaan yang anda lakukan adalah = %d", attempt);
-        }
+                printf("\nJawaban anda terlalu Tinggi!!\n");
+            } 
+        else {
+             printf("\nSelamat!!!\njawaban anda %d benar!! angka yang benar adalah: %d", guess, answer);
+        }}
+    while (guess != answer);
+            printf("\n\nAngka yang benar = %d", answer);
+            printf("\njumlah percobaan yang anda lakukan adalah = %d", attempt);
+        
 
-    }
+    
     
 
 
---
+++ practice_project/rock_paper_scisscort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+
+int main () {
+
+    printf("\n\n****** Selamat datang di game Batu gunting kertas !! *******");
+    
+
+
+
+
+
+
+
+    return 0
+}
